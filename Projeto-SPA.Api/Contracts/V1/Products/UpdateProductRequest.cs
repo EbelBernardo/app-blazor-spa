f78a3ae7d5c34b1ff2f5913ac015371e23181bca
Bernardo Ebel <bernardoebel@gmail.com>
2026-03-04 16:31:40 -0300

    feat(api): introduce request and response DTOs for categories and products

---
+++ Projeto-SPA.Api/Contracts/V1/Products/UpdateProductRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto_SPA.Api.Contracts.V1.Products
+{
+    public class UpdateProductRequest
+    {
+        public required string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}

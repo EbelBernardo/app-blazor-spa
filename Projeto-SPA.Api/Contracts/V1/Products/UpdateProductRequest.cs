5f9ea7b157d5d9111f816a877c401fc5ed2621d0
Bernardo Ebel <bernardoebel@gmail.com>
2026-03-10 19:01:37 -0300

    feat(api): implement full CRUD for product endpoints

--- Projeto-SPA.Api/Contracts/V1/Products/UpdateProductRequest.cs
+++ Projeto-SPA.Api/Contracts/V1/Products/UpdateProductRequest.cs
@@ -1,11 +1,20 @@
-﻿namespace Projeto_SPA.Api.Contracts.V1.Products
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_SPA.Api.Contracts.V1.Products
 {
     public class UpdateProductRequest
     {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 2,
+            ErrorMessage = "Title must be between 2 and 100 characters")]
         public required string Title { get; set; }
 
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Price must be greater than 0")]
         public decimal Price { get; set; }
 
+        [Range(1, int.MaxValue,
+            ErrorMessage = "CategoryId must be a valid id")]
         public int CategoryId { get; set; }
     }
 }
